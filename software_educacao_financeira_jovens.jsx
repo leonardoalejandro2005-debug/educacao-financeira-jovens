fe4a87d827b6772af8be6d63f7b8e1b2b02d60c9
leonardoalejandro2005-debug <leonardoalejandro2005@gmail.com>
2026-05-25 14:24:03 -0300

    Add files via upload

---
+++ software_educacao_financeira_jovens.jsx
@@ -0,0 +1,8 @@
+export default function FinancialEducationProject() {
+  return (
+    <div>
+      <h1>Educação Financeira para Jovens</h1>
+      <p>Projeto educativo para conscientização financeira.</p>
+    </div>
+  );
+}
